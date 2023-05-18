2fc1081d0ac59a8ef65683bbcecb64229d50b26e
md-toybur-rahman <toyburrahman735@gmail.com>
2023-05-18 09:03:58 +0600

    made folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <div data-theme="light">
+    <div>
       <h3 className='text-7xl bg-red-500 text-center'>Hello</h3>
     </div>
   )
---
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="bg-red-500">
+            <h1>This is Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
