c7de4f2d6d1cf8097be17cea123d0d5141214a6a
md-toybur-rahman <toyburrahman735@gmail.com>
2023-05-18 08:18:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div data-theme="light">
+      <h3 className='text-7xl bg-red-500 text-center'>Hello</h3>
+    </div>
+  )
+}
+
+export default App
